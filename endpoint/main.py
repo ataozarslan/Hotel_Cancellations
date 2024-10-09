168b232e7eb4fb52bc68ec350f966e2a45ac9f45
Ata Özarslan <ataozarslan@hotmail.com>
2024-10-10 02:04:44 +0300

    Add files via upload

--- endpoint/main.py
+++ endpoint/main.py
@@ -1,34 +1,27 @@
 from fastapi import FastAPI
-from joblib import load
-import numpy as np
 from pydantic import BaseModel
+import pandas as pd
+from joblib import load
+
+xgb_model = load('hotel_cancellation_model.pkl')
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return 'Welcome to Hotel Cancellation Prediction API!'
+class PredictionInput(BaseModel):
+    lead_time: int
+    no_of_special_requests: int
+    avg_price_per_room: float
+    market_segment_type_Online: int
+    arrival_month: int
+    arrival_date: int
+    arrival_year: int
+    no_of_weekend_nights: int
+    no_of_week_nights: int
+    no_of_adults: int
 
-class MyData(BaseModel):
-    lead_time:int
-    no_of_special_requests:int
-    avg_price_per_room:int
-    market_segment_type_Online:int
-    arrival_month:int
-    arrival_date:int
-    arrival_year:int
-    no_of_weekend_nights:int
-    no_of_week_nights:int
-    no_of_adults:int
-
-@app.post('/predict')
-def predict(data:MyData):
-    model = load('xgb_model.pkl')
-    features = np.array([[data.lead_time, data.no_of_special_requests, data.avg_price_per_room,
-                          data.market_segment_type_Online, data.arrival_month, data.arrival_date,
-                          data.arrival_year, data.no_of_weekend_nights, data.no_of_week_nights, data.no_of_adults]])
-    prediction = (model.predict_proba(features)[:,1] > 0.3).astype(int)
-    prediction_proba = model.predict_proba(features)[:,1]
-    return {"Is Canceled":int(prediction), "Cancelation Probability (%)":float(prediction_proba*100)}
-
-# python -m uvicorn main:app --reload
+@app.post("/predict")
+async def predict(input_data: PredictionInput):
+    input_df = pd.DataFrame([input_data.dict()])
+    prediction = (xgb_model.predict_proba(input_df)[:,1] > 0.3).astype(int)
+    probability = xgb_model.predict_proba(input_df)[0][1] * 100
+    return {"prediction": "Will be Canceled" if prediction[0] else "Will Not be Canceled", "probability": probability}
