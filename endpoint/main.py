96b0af63fdde440afa8c683c165aca6f970336b5
Ata Özarslan <ataozarslan@hotmail.com>
2024-10-12 14:57:18 +0300

    Prediction API updated

--- endpoint/main.py
+++ endpoint/main.py
@@ -1,11 +1,15 @@
+import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
-import pandas as pd
 from joblib import load
 
 xgb_model = load('hotel_cancellation_model.pkl')
 
 app = FastAPI()
+
+@app.get('/')
+def home():
+    return 'Welcome to Hotel Cancellation Prediction API!'
 
 class PredictionInput(BaseModel):
     lead_time: int
@@ -21,7 +25,23 @@ class PredictionInput(BaseModel):
 
 @app.post("/predict")
 async def predict(input_data: PredictionInput):
-    input_df = pd.DataFrame([input_data.dict()])
-    prediction = (xgb_model.predict_proba(input_df)[:,1] > 0.3).astype(int)
-    probability = xgb_model.predict_proba(input_df)[0][1] * 100
-    return {"prediction": "Will be Canceled" if prediction[0] else "Will Not be Canceled", "probability": probability}
+
+    inputs = [[
+        input_data.lead_time,
+        input_data.no_of_special_requests,
+        input_data.avg_price_per_room,
+        input_data.market_segment_type_Online,
+        input_data.arrival_month,
+        input_data.arrival_date,
+        input_data.arrival_year,
+        input_data.no_of_weekend_nights,
+        input_data.no_of_week_nights,
+        input_data.no_of_adults
+    ]]
+
+    probabilities = xgb_model.predict_proba(inputs)
+    prediction = np.where(probabilities[:,1] >= 0.3, 'Will be Canceled!', 'Will Not be Canceled!')[0]
+    probability = probabilities[0, 1]
+    return {"Is Canceled": prediction, "Cancelation Probability (%)": float(probability)}
+
+# python -m uvicorn main:app --reload
